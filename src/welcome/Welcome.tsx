09ae5bd4ab8bad2764f1f5c91d2570fe061a5f24
MasanoriIwakura <hagobutler@yahoo.co.jp>
2023-04-03 09:24:04 +0900

    feat: update welcome page

--- src/welcome/Welcome.tsx
+++ src/welcome/Welcome.tsx
@@ -3,7 +3,7 @@ import React, { ReactElement } from 'react';
 const Welcome = (): ReactElement => {
   return (
     <div className="flex h-screen items-center justify-center">
-      <h1>Welcome</h1>
+      <h1>Welcome to king-of-iruca extension.</h1>
     </div>
   );
 };
